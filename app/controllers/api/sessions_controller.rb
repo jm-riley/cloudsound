cf2d767c75c747629e796961bda9b2f3837b1fed
John-Michael Riley <johnmichael.w.riley@gmail.com>
2019-05-20 15:51:24 -0400

    added backend routes and controllers for users and session

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      render :show
+    else
+      render json: ["Invalid username or password"], status: 422
+    end
+  end
+
+  def destroy
+    if !logged_in?
+      render json: ["Not logged in"], status: 404
+    end
+    logout
+    render json: {}
+  end
+
+
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+  helper_methods :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
